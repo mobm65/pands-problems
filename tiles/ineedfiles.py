55238c09de1e61cf8bed4b69b1a5342cf01387ac
mobm65 <60409970+mobm65@users.noreply.github.com>
2020-02-29 19:21:35 +0000

    measure for tiles.

---
+++ tiles/ineedfiles.py
@@ -0,0 +1,11 @@
+# this program calculates how many tiles you will need
+# when tiling a wall or floor (in m2)
+
+length = 4.0
+width = 2.0
+
+area = length * width
+
+needed = area * 1.05
+
+print("you need", needed, "tiles in metres squared")
